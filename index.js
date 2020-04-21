9f8c521d9ae3af108af95fa1ca20ffaf0f2bc7f9
Vijay Singh <vj2010sn@gmail.com>
2020-04-21 12:52:34 +0530

    Feedback fixes js (#1)
    * feedback fixes for js plugins
    
    * readme added
    
    Co-authored-by: vijay.singh <vijay.singh@inmobi.com>

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ const babel = require("@babel/core");
 const glob = require("glob");
 const fs = require("fs");
 const Store = require("./store");
+const argv = require("yargs").argv;
+const sourcePath = argv.src;
+const outputFileName = argv.src + "-output" || "output";
 
 // add a project here in store
-Store.addProject("proj1");
+Store.addProject(sourcePath);
 
-glob("src/*.js", {}, function (err, files) {
+glob(sourcePath + "/**/*+(.js|.jsx|.ts|.tsx)", {}, function (err, files) {
   files.forEach((filePath) => {
     babel.transform(fs.readFileSync(filePath, "utf8"), {
       sourceType: "module",
       plugins: [
-        "@babel/plugin-syntax-jsx",
+        "@babel/plugin-proposal-class-properties",
         [
           "./plugins/babel-plugin-import.js",
           {
             importName: ["ireact-components", "chromos"],
             cb: (val, name) => {
-              Store.updateImportCollection("proj1", val, name);
+              Store.updateImportCollection(sourcePath, val, name);
             },
           },
         ],
@@ -25,7 +28,7 @@ glob("src/*.js", {}, function (err, files) {
           "./plugins/babel-plugin-native-element.js",
           {
             cb: (el) => {
-              Store.updateNativeElementList("proj1", el);
+              Store.updateNativeElementList(sourcePath, el);
             },
           },
         ],
@@ -33,21 +36,33 @@ glob("src/*.js", {}, function (err, files) {
           "./plugins/babel-plugin-classname.js",
           {
             cb: (className) => {
-              Store.updateClassNameList("proj1", className);
+              Store.updateClassNameList(sourcePath, className);
             },
           },
         ],
         [
-          "./plugins/babel-plugin-styleprop.js",
+          "./plugins/babel-plugin-getprop.js",
           {
-            cb: () => {
-              Store.updateStyleNamePropCount("proj1");
+            propList: ["style", "className"],
+            cb: (prop) => {
+              Store.updateStyleNamePropCount(sourcePath);
             },
+          },
+        ],
+      ],
+      presets: [
+        "@babel/preset-env",
+        "@babel/preset-react",
+        [
+          "@babel/preset-typescript",
+          {
+            isTSX: true,
+            allExtensions: true,
           },
         ],
       ],
     });
   });
 
-  fs.writeFileSync("output.json", JSON.stringify(Store.getStore()));
+  fs.writeFileSync(outputFileName + ".json", JSON.stringify(Store.getStore()));
 });
